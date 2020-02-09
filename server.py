08c021f9ebf5eaaf25487a6abfb26e8d2904eaee
Zhang <504947985@qq.com>
2020-02-09 22:34:35 +0800

    use socket to create TCP server and update README.md

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 #coding=utf8
 
-from socketserver import StreamRequestHandler,TCPServer
+import socket
 
-class EchoHandler(StreamRequestHandler):
+def echo_handler(address,client_sock):
+    print('Got connection from {}'.format(address))
+    while True:
+        msg = client_sock.recv(8192).strip()
+        client_sock.sendall(msg)
+    client_sock.close()
 
-    def handle(self):
-        print('Got connect from {}'.format(self.client_address))
-        # self.rfile is a file-like object for reading
-        self.data = self.rfile.readline().strip()
-        print(self.data.decode())
-        self.wfile.write(self.data)
-#        for line in self.rfile:
-#            print('Recv {} from {}'.format(line.decode(),self.client_address)) 
-            # self.wfile is a file-like object for writing
-#            self.wfile.write(line)
+def echo_server(address,backlog=5):
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.bind(address)
+    sock.listen(backlog)
+    while True:
+        client_sock,client_addr = sock.accept()
+        echo_handler(client_addr,client_sock)
 
 if __name__ == '__main__':
-    s = TCPServer(('',11111),EchoHandler)
-    s.serve_forever()
-
+    echo_server(('',11111))
